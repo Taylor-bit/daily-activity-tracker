bf8f1136ecb0986b8756f87a7806686c1c25baff
TaylorColton <coltontaylor775@gmail.com>
2023-05-21 23:23:41 -0700

    Updates

---
+++ precache-manifest.c0085bca43467828ef7f6f3db006f0be.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "432ecc57ef0c375dd241",
+    "url": "/daily-activity-tracker/static/css/main.f13c9388.chunk.css"
+  },
+  {
+    "revision": "432ecc57ef0c375dd241",
+    "url": "/daily-activity-tracker/static/js/main.432ecc57.chunk.js"
+  },
+  {
+    "revision": "3bc8ac50b02e373cc067",
+    "url": "/daily-activity-tracker/static/js/1.3bc8ac50.chunk.js"
+  },
+  {
+    "revision": "037da65a3f1366d425ee",
+    "url": "/daily-activity-tracker/static/js/2.037da65a.chunk.js"
+  },
+  {
+    "revision": "88ddee98a1354e9964ae",
+    "url": "/daily-activity-tracker/static/js/runtime~main.88ddee98.js"
+  },
+  {
+    "revision": "648467449470f31ea5401f155d56204e",
+    "url": "/daily-activity-tracker/index.html"
+  }
+];
